82a99d0c8596f5ed454f307c2ee4165a1d4551ec
xen <romainrobert.xen@gmail.com>
2025-03-24 10:08:08 +0100

    Update my_ls.c

--- C/qwasar/projet/season01_arc02/my_ls/my_ls.c
+++ C/qwasar/projet/season01_arc02/my_ls/my_ls.c
@@ -10,32 +10,58 @@
 void print(char **str) {
     int i = 0;
     while (str[i] != NULL) {
-        printf("%s\t", str[i]);
+        printf("%s\n", str[i]);
         i++;
     }
-    printf("\n");
+    // printf("\n");
 }
 
-void get_sorted_files_by_character(char **str) {
-    for(int i = 0; i < strlen(str); i++) {
-        for(int j = 0; j < strlen(str) - i - 1; j++) {
-            if(str[j][0] > str[j+1][0]) {
-                char *temp = str[j];
-                str[j] = str[j+1];
-                str[j+1] = temp;
+/*if(str[index][2] > str[index + 1][2]) {
+            temp = str[index];
+            str[index] = str[index + 1];
+            str[index + 1] = temp;
+        }*/
+
+void get_sorted_files_by_character(char **str, int size) {
+    int index = 0;
+    char *temp;
+    int n;
+
+    while(str[index] != NULL) {
+        n = '\0';
+        for(int i = 0; i < size; i++) {
+            if(str[i][2] > str[index][2]) {
+                n = i;
             }
         }
+        if(n != '\0') {
+            temp = str[index];
+            str[index] = str[n];
+            str[n] = temp;
+        }
+        index++;
     }
     print(str);
 }
+// void get_sorted_files_by_times(char **str, int *times) {
+// }
 
+char **str_with_true_length(int size, char **str) {
+    char **result = malloc(sizeof(char *) * size);
+    for(int i = 0; i < size; i++) {
+        result[i] = str[i];
+    }
 
-void get_all_files(char *c) {
+    return result;
+}
+
+void get_all_files() {
     DIR *dir;
     struct dirent *entry;
     dir = opendir(".");
-    char **str = malloc(sizeof(char *) * 100);
+    char **str = malloc(sizeof(char *) * 50);
     int index = 0;
+    int count = 0;
 
     if (dir == NULL) {
         printf("Unable to open directory");
@@ -43,41 +69,52 @@ void get_all_files(char *c) {
     }
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_name[0] != '.') {
-            str[index++] = strdup(entry->d_name);
+            str[index++] = entry->d_name;
+            count++;
         }
     }
-    get_sorted_files_by_character(str);
+
+    char **result = str_with_true_length(count, str);
+    free(str);
+
+    get_sorted_files_by_character(result, count);
 
     closedir(dir);
-    free(str);
     // printf("Print all files\n");
 }
 
-void flag_a(char *c) {     // print_all_files_by_dot
+void flag_a() {     // print_all_files_by_dot
     DIR *dir;
     struct dirent *entry;
     dir = opendir(".");
-    char **str = malloc(sizeof(char *) * 100);
+    char **str = malloc(sizeof(char *) * 50);
     int index = 0;
+    int count = 0;
 
     if (dir == NULL) {
         printf("Unable to open directory");
         return;
     }
+
     while ((entry = readdir(dir)) != NULL) {
-        str[index++] = strdup(entry->d_name);
+        str[index++] = entry->d_name;
+        count++;
     }
-    get_sorted_files_by_character(str);
+
+    char **result = str_with_true_length(count, str);
+    free(str);
+
+    get_sorted_files_by_character(result, count);
 
     closedir(dir);
-    free(str);
     // printf("Flag -a\n");
 }
-void flag_t(char *c) {     // print_all_files_by_time
+void flag_t() {     // print_all_files_by_time
     DIR *dir;
     struct dirent *entry;
+    struct stat st;
     dir = opendir(".");
-    char **str = malloc(sizeof(char *) * 100);
+    char **str = malloc(sizeof(char *) * 50);
     int index = 0;
 
     if (dir == NULL) {
@@ -86,7 +123,8 @@ void flag_t(char *c) {     // print_all_files_by_time
     }
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_name[0] != '.') {
-            str[index++] = strdup(entry->d_name);
+            stat(entry->d_name, &st);
+            str[index++] = entry->d_name;
         }
     }
     print(str);
@@ -96,8 +134,7 @@ void flag_t(char *c) {     // print_all_files_by_time
     // printf("Flag -t\n");
 }
 
-void flag_at(char *c) {    // print_all_files_by_time_and_by_dot
-    printf("%s", c);
+void flag_at() {    // print_all_files_by_time_and_by_dot
     printf("Flag -at\n");
 }
 
@@ -106,7 +143,6 @@ void get_flag(int ac, char** av) {
     int index_flag_a = 0;
     int index_flag_t = 0;
     int index_flag_at = 0;
-    char *c = ".";
 
     while(index < ac) {
         if(strcmp("-at", av[index]) == 0 || strcmp("-ta", av[index]) == 0) {
@@ -115,21 +151,18 @@ void get_flag(int ac, char** av) {
             index_flag_a = 1;
         } else if(strcmp("-t", av[index]) == 0) {
             index_flag_t = 1;
-        } else {
-            c = av[index];
-            printf("%s:\n", c);
         }
         index += 1;
     }
 
     if((index_flag_a == 1 && index_flag_t == 1) || index_flag_at == 1) {
-        flag_at(c);
+        flag_at();
     } else if(index_flag_a == 1 && index_flag_t == 0) {
-        flag_a(c);
+        flag_a();
     } else if(index_flag_t == 1 && index_flag_a == 0) {
-        flag_t(c);
+        flag_t();
     } else {
-        get_all_files(c);
+        get_all_files();
     }
 }
 
